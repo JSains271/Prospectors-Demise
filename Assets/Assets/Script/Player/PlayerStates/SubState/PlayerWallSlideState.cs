68b22c95ef9981efb64e13dca2672677a16ad997
JSains271 <521178@chrome.escg.ac.uk>
2023-05-05 16:15:05 +0100

    Doing Player controler
    video at 27:11, part 25

---
+++ Assets/Assets/Script/Player/PlayerStates/SubState/PlayerWallSlideState.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class PlayerWallSlideState : PlayerTouchingWallState
+{
+    public PlayerWallSlideState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (!isExitingState)
+        {
+            player.SetVelocityY(-playerData.wallSlideVelocity);
+
+            if (grabInput && yInput == 0)
+            {
+                stateMachine.ChangeState(player.WallGrabState);
+            }
+        }
+    }
+}
